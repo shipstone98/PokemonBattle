860be305e8661f7d695b73cb4bba9e2dbfb3c97e
Christopher Shipstone <christopher.shipstone98@gmail.com>
2026-05-20 12:00:14 +0100

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.3
+
+import PackageDescription
+
+let package = Package(
+    name: "PokemonBattle",
+    products: [
+        .library(name: "PokemonBattle", targets: ["PokemonBattle"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/shipstone98/Utilities", from: "1.0.1")
+    ],
+    targets: [
+        .target(name: "PokemonBattle", dependencies: ["Utilities"]),
+        .testTarget(
+            name: "PokemonBattleTests",
+            dependencies: ["PokemonBattle"]
+        )
+    ],
+    swiftLanguageModes: [.v6]
+)
